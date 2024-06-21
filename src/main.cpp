24b613ea97fb0a25e831c4011303d6c52e0b0d5c
Léo Hervouet <leoohervouet@gmail.com>
2024-06-21 14:29:23 +0200

    Init Commit

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,14 @@
 
 int main()
 {
-    auto window = sf::RenderWindow{ { 1920u, 1080u }, "CMake SFML Project" };
+    sf::RenderWindow window = sf::RenderWindow{ sf::VideoMode{ 1920u, 1080u },
+                                                "SFML Test project" };
     window.setFramerateLimit(144);
+    window.setPosition(sf::Vector2i(50, 50));
 
     while (window.isOpen())
     {
-        for (auto event = sf::Event{}; window.pollEvent(event);)
+        for (sf::Event event = sf::Event{}; window.pollEvent(event);)
         {
             if (event.type == sf::Event::Closed)
             {
